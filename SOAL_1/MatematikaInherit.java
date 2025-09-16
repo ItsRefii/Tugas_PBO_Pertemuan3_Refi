cd3625adb14c03a616ac24ecc59ebb45343ae4f4
ItsRefii <refiabdillah19@gmail.com>
2025-09-16 17:07:27 +0700

    MatematikaInherit.java

--- SOAL_1/MatematikaInherit.java
+++ SOAL_1/MatematikaInherit.java
@@ -6,12 +6,13 @@ public class MatematikaInherit {
         System.out.println("     PROGRAM MATEMATIKA INHERITANCE      ");
         System.out.println("=========================================");
         
-        System.out.println("| Tambah  : " + mtk.tambah(13, 5) + "                   |");
-        System.out.println("| Kurang  : " + mtk.kurang(16, 3) + "                   |");
-        System.out.println("| Kali    : " + mtk.kali(9, 7) + "                   |");
-        System.out.println("| Bagi    : " + mtk.bagi(50, 5) + "                  |");
+        System.out.println("| Tambah  : " + mtk.tambah(72, 13) + "                   |");
+        System.out.println("| Kurang  : " + mtk.kurang(19, 7) + "                   |");
+        System.out.println("| Kali    : " + mtk.kali(7, 7) + "                   |");
+        System.out.println("| Bagi    : " + mtk.bagi(45, 5) + "                  |");
         System.out.println("| Modulus : " + mtk.modulus(10, 5) + "                    |");
         
         System.out.println("=========================================");
     }
+
 }   
